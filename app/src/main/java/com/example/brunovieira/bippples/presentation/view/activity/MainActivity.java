31839f6f9a2cc665ff11587c8352b0b61c6177c7
bruno.vieira <bruno.vieira@concretesolutions.com.br>
2016-03-28 18:42:02 -0300

    Starting project

---
+++ app/src/main/java/com/example/brunovieira/bippples/presentation/view/activity/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.brunovieira.bippples.presentation.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.brunovieira.bippples.R;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+}
