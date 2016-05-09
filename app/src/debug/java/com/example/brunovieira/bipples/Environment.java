8b7081145a0d48e13db477dcc52ddcd8c9795e03
bruno.vieira <bruno.vieira@concretesolutions.com.br>
2016-05-09 09:23:26 -0300

    added loglevel

--- app/src/debug/java/com/example/brunovieira/bipples/Environment.java
+++ app/src/debug/java/com/example/brunovieira/bipples/Environment.java
@@ -1,5 +1,7 @@
 package com.example.brunovieira.bipples;
 
+import okhttp3.logging.HttpLoggingInterceptor;
+
 /**
  * Created by bruno.vieira on 03/05/2016.
  */
@@ -7,4 +9,5 @@ public class Environment {
     public static final String RANDOM_JOKE_URL = "http://api.icndb.com/jokes/random";
     public static final String SERVER_URL = "https://api.dribbble.com/v1/";
     public static final String ACCESS_TOKEN = "a021928e475b71c91b93eeaab188013bb81b8b7c49c042d63f6176a362793bd3";
+    public static final HttpLoggingInterceptor.Level LOG_LEVEL = HttpLoggingInterceptor.Level.BODY;
 }
